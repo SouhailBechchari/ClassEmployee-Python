628db86a8432d34afb3f6245d49022f38a11dc5b
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-11-19 22:56:42 +0100

    Create ClassEmployee.py

---
+++ ClassEmployee.py
@@ -0,0 +1,11 @@
+class Employee:
+    
+    def __init__(self,baseSalary,bonusHrs,sales,name):
+        self.baseSalary = baseSalary
+        self.bonusHrs = bonusHrs
+        self.sales = sales
+        self.name = name
+
+    def CalculateNetSalary():
+        pass
+  
